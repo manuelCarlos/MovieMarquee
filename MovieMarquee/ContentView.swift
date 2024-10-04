6aa3d3b5422a834b83d91f23eb0695e2a2c70cc6
Manuel Lopes <manuelcarloslopes@icloud.com>
2024-10-04 14:16:10 +0200

    Add Inital Xcode project setup

---
+++ MovieMarquee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieMarquee
+//
+//  Created by Manuel Lopes on 04.10.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieMarquee/MovieMarqueeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMarqueeApp.swift
+//  MovieMarquee
+//
+//  Created by Manuel Lopes on 04.10.24.
+//
+
+import SwiftUI
+
+@main
+struct MovieMarqueeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
