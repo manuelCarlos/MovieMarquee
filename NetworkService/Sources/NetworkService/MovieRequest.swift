263706b273d3728449b42febbd85c513d1e7b65a
Manuel Lopes <manuelcarloslopes@icloud.com>
2024-10-08 19:58:24 +0200

    Add MovieRequest implementations and unit tests

---
+++ NetworkService/Sources/NetworkService/MovieRequest.swift
@@ -0,0 +1,83 @@
+//
+//  MovieRequest.swift
+//  NetworkService
+//
+//  Created by Manuel Lopes on 08.10.24.
+//
+
+enum MovieRequest: MediaTypeProtocol {
+    case getPopularMovies(page: Int)
+    case getUpcomingMovies(page: Int)
+    case getNowPlayingMovies(page: Int)
+    case getMovieDetail(id: Int)
+    case getMovieReviews(id: Int, page: Int)
+    case getSimilarMoviesTo(id: Int, page: Int)
+    case getCredits(id: Int)
+
+    var mediaType: MediaType { .movie }
+
+    var path: String {
+        switch self {
+        case .getPopularMovies:
+            return "/movie/popular"
+        case .getUpcomingMovies:
+            return "/movie/upcoming"
+        case .getNowPlayingMovies:
+            return "/movie/now_playing"
+        case .getMovieDetail(id: let id):
+            return "/movie/\(id)"
+        case .getMovieReviews(let id, _):
+            return "/movie/\(id)/reviews"
+        case .getSimilarMoviesTo(let id, _):
+            return "/movie/\(id)/similar"
+        case .getCredits(id: let id):
+            return "/movie/\(id)/credits"
+        }
+    }
+
+    var urlParams: [String: String?] {
+        switch self {
+        case .getPopularMovies(let page):
+            return ["page": String(page),
+                    "api_key": APIConstants.apiKey,
+                    "language": APIConstants.language
+            ]
+        case .getUpcomingMovies(let page):
+            return ["page": String(page),
+                    "api_key": APIConstants.apiKey,
+                    "language": APIConstants.language
+            ]
+        case .getNowPlayingMovies(let page):
+            return ["page": String(page),
+                    "api_key": APIConstants.apiKey,
+                    "language": APIConstants.language
+            ]
+        case .getMovieDetail(id: let id):
+            return ["movie_id": String(id),
+                    "api_key": APIConstants.apiKey,
+                    "append_to_response": "credits"
+            ]
+        case .getMovieReviews(let id, let page):
+            return ["movie_id": String(id),
+                    "api_key": APIConstants.apiKey,
+                    "language": APIConstants.language,
+                    "page": String(page)
+            ]
+        case .getSimilarMoviesTo(let id, let page):
+            return ["movie_id": String(id),
+                    "api_key": APIConstants.apiKey,
+                    "language": APIConstants.language,
+                    "page": String(page)
+            ]
+        case .getCredits(id: let id):
+            return ["movie_id": String(id),
+                    "api_key": APIConstants.apiKey,
+                    "language": APIConstants.language
+            ]
+        }
+    }
+
+    var requestType: RequestType {
+        .GET
+    }
+}
---
+++ NetworkService/Tests/NetworkServiceTests/MovieRequestTests.swift
@@ -0,0 +1,119 @@
+//
+//  MovieRequestTests.swift
+//  NetworkService
+//
+//  Created by Manuel Lopes on 08.10.24.
+//
+
+import XCTest
+@testable import NetworkService
+
+final class MovieRequestTests: XCTestCase {
+
+    func test_movie_request_get_popular_movies_path_should_be_correct() {
+        let request = MovieRequest.getPopularMovies(page: 1)
+
+        let path = request.path
+
+        XCTAssertEqual(path, "/movie/popular")
+    }
+
+    func test_movie_request_get_upcoming_movies_path_should_be_correct() {
+        let request = MovieRequest.getUpcomingMovies(page: 1)
+
+        let path = request.path
+
+        XCTAssertEqual(path, "/movie/upcoming")
+    }
+
+    func test_movie_request_get_now_playing_movies_path_should_be_correct() {
+        let request = MovieRequest.getNowPlayingMovies(page: 1)
+
+        let path = request.path
+
+        XCTAssertEqual(path, "/movie/now_playing")
+    }
+
+    func test_movie_request_get_movie_detail_path_should_be_correct() {
+        let request = MovieRequest.getMovieDetail(id: 123)
+
+        let path = request.path
+
+        XCTAssertEqual(path, "/movie/123")
+    }
+
+    func test_movie_request_get_movie_reviews_path_should_be_correct() {
+        let request = MovieRequest.getMovieReviews(id: 123, page: 1)
+
+        let path = request.path
+
+        XCTAssertEqual(path, "/movie/123/reviews")
+    }
+
+    func test_movie_request_get_similar_movies_to_path_should_be_correct() {
+        let request = MovieRequest.getSimilarMoviesTo(id: 123, page: 1)
+
+        let path = request.path
+
+        XCTAssertEqual(path, "/movie/123/similar")
+    }
+
+    func test_movie_request_get_credits_path_should_be_correct() {
+        let request = MovieRequest.getCredits(id: 123)
+
+        let path = request.path
+
+        XCTAssertEqual(path, "/movie/123/credits")
+    }
+
+    func test_movie_request_get_popular_movies_url_params_should_include_page_and_api_key() {
+        let page = 1
+        let request = MovieRequest.getPopularMovies(page: page)
+
+        let urlParams = request.urlParams
+
+        XCTAssertEqual(urlParams["page"], String(page))
+        XCTAssertEqual(urlParams["api_key"], APIConstants.apiKey)
+        XCTAssertEqual(urlParams["language"], APIConstants.language)
+    }
+
+    func test_movie_request_get_movie_detail_url_params_should_include_movie_id_and_api_key() {
+        let movieId = 123
+        let request = MovieRequest.getMovieDetail(id: movieId)
+
+        let urlParams = request.urlParams
+
+        XCTAssertEqual(urlParams["movie_id"], String(movieId))
+        XCTAssertEqual(urlParams["api_key"], APIConstants.apiKey)
+        XCTAssertEqual(urlParams["append_to_response"], "credits")
+    }
+
+    func test_movie_request_get_movie_reviews_url_params_should_include_movie_id_page_and_api_key() {
+        let movieId = 123
+        let page = 1
+        let request = MovieRequest.getMovieReviews(id: movieId, page: page)
+
+        let urlParams = request.urlParams
+
+        XCTAssertEqual(urlParams["movie_id"], String(movieId))
+        XCTAssertEqual(urlParams["page"], String(page))
+        XCTAssertEqual(urlParams["api_key"], APIConstants.apiKey)
+        XCTAssertEqual(urlParams["language"], APIConstants.language)
+    }
+
+    func test_movie_request_request_type_should_be_get() {
+        let requests: [MovieRequest] = [
+            .getPopularMovies(page: 1),
+            .getUpcomingMovies(page: 1),
+            .getNowPlayingMovies(page: 1),
+            .getMovieDetail(id: 123),
+            .getMovieReviews(id: 123, page: 1),
+            .getSimilarMoviesTo(id: 123, page: 1),
+            .getCredits(id: 123)
+        ]
+
+        for request in requests {
+            XCTAssertEqual(request.requestType, .GET)
+        }
+    }
+}
