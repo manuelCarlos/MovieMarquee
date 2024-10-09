4cbdc99e08f56e4da237fbc0b59468cc857691af
Manuel Lopes <manuelcarloslopes@icloud.com>
2024-10-09 13:18:53 +0200

    Add OriginalLanguage model unit tests

---
+++ Models/Tests/ModelsTests/OriginalLanguageTests.swift
@@ -0,0 +1,43 @@
+//
+//  OriginalLanguageTests.swift
+//  Models
+//
+//  Created by Manuel Lopes on 09.10.24.
+//
+
+import XCTest
+
+@testable import Models
+
+final class OriginalLanguageTests: XCTestCase {
+
+    func test_init_with_valid_language_name() {
+        let language = OriginalLanguage(language: "English")
+        XCTAssertEqual(language, .en)
+    }
+
+    func test_init_with_invalid_language_name() {
+        let language = OriginalLanguage(language: "InvalidLanguage")
+        XCTAssertNil(language)
+    }
+
+    func test_init_with_valid_iso_code() {
+        let language = OriginalLanguage(isoCode: "en")
+        XCTAssertEqual(language, .en)
+    }
+
+    func test_init_with_invalid_iso_code() {
+        let language = OriginalLanguage(isoCode: "xxl")
+        XCTAssertNil(language)
+    }
+
+    func test_language_property() {
+        let language = OriginalLanguage.en
+        XCTAssertEqual(language.language, "English")
+    }
+
+    func test_all_cases_count() {
+        let allLanguages = OriginalLanguage.allCases
+        XCTAssertEqual(allLanguages.count, 187)
+    }
+}
