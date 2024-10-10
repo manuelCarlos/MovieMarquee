0847bd417f14fc54a9dfa07994131341de011122
Manuel Lopes <manuelcarloslopes@icloud.com>
2024-10-10 17:13:46 +0200

    Add MediaService protocol

---
+++ MovieMarquee/MediaService.swift
@@ -0,0 +1,13 @@
+//
+//  MediaService.swift
+//  MovieMarquee
+//
+//  Created by Manuel Lopes on 10.10.24.
+//
+
+import NetworkService
+
+// TODO: - Rename
+protocol MediaService: Sendable {
+    func fetchMedia(request: NetworkRequest) async throws -> [Watchable]
+}
