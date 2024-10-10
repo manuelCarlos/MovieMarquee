fae6b3de2b43073dbccd246bac346410a070950b
Manuel Lopes <manuelcarloslopes@icloud.com>
2024-10-10 16:50:45 +0200

    Add MediaInteractor protocol and default implementation

---
+++ MovieMarquee/MediaInteractor.swift
@@ -0,0 +1,24 @@
+//
+//  MediaInteractor.swift
+//  MovieMarquee
+//
+//  Created by Manuel Lopes on 10.10.24.
+//
+
+import NetworkService
+
+// TODO: - Rename file and MediaInteractor
+
+protocol MediaInteractor: Sendable {
+    func fetchNextPopularPageAsFullList() async throws -> [Watchable]
+}
+
+final class DefaultMediaInteractor: Sendable, MediaInteractor {
+
+    let popularMoviesFetcher: Fetchable = MediaFetcher(mediaListFetcher: PopularMoviesFetcher(),
+                                                       service: MovieService.shared)
+
+    func fetchNextPopularPageAsFullList() async throws -> [Watchable] {
+        return try await popularMoviesFetcher.fetchWithNextPage()
+    }
+}
