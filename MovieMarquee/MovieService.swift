f8f529d6cbc58669f0e60c5ddc184a90d5137031
Manuel Lopes <manuelcarloslopes@icloud.com>
2024-10-10 17:03:16 +0200

    Add MovieService class implementation

---
+++ MovieMarquee/MovieService.swift
@@ -0,0 +1,24 @@
+//
+//  MovieService.swift
+//  MovieMarquee
+//
+//  Created by Manuel Lopes on 10.10.24.
+//
+
+import NetworkService
+import Models
+
+final class MovieService: Sendable, MediaService {
+    private let requestManager: RequestManagerProtocol
+
+    public static let shared = MovieService()
+
+    init(requestManager: RequestManagerProtocol = RequestManager()) {
+        self.requestManager = requestManager
+    }
+
+    func fetchMedia(request: NetworkRequest) async throws -> [Watchable] {
+        let movies: Movies = try await requestManager.decode(networkRequest: request)
+        return movies.results
+    }
+}
