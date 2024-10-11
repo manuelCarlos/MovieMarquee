e179a12833bd927034ae24daec60c6cd91b47138
Manuel Lopes <manuelcarloslopes@icloud.com>
2024-10-11 19:35:50 +0200

    Add MediaListItemView implementation

--- MovieMarquee/UI/MediaListItemView.swift
+++ MovieMarquee/UI/MediaListItemView.swift
@@ -1,71 +1,65 @@
+//
+//  MediaListItemView.swift
+//  MovieMarquee
+//
+//  Created by Manuel Lopes on 11.10.24.
+//
+
+import SwiftUI
+
 struct MediaListItemView: View {
 
-    var mediaListItem: Watchable
+    private let mediaItem: Watchable
 
-    internal init(mediaListItem: Watchable) {
-        self.mediaListItem = mediaListItem
+    init(mediaItem: Watchable) {
+        self.mediaItem = mediaItem
+    }
+
+    var body: some View {
+        NavigationLink(
+            destination:
+                getMediaDetailView()
+        ) {
+            HStack(alignment: .center, spacing: 20) {
+                MoviePosterView(item: mediaItem)
+                    .frame(maxWidth: 150)
+                movieInfoStackView
+                Spacer()
+            }
+            .frame(height: 250)
+        }
     }
 
     @ViewBuilder
     private var movieInfoStackView: some View {
         VStack(alignment: .leading, spacing: 5) {
-            Text(self.mediaListItem.title)
+            Text(self.mediaItem.title)
                 .font(.title3)
                 .minimumScaleFactor(0.7)
                 .bold()
                 .foregroundColor(.primary)
                 .multilineTextAlignment(.leading)
 
-            if let originalLanguage = self.mediaListItem.originalLanguage?.language {
+            if let originalLanguage = mediaItem.originalLanguage?.language {
                 Text(originalLanguage)
                     .font(.title3)
                     .minimumScaleFactor(0.7)
                     .bold()
                     .foregroundColor(.primary)
                     .multilineTextAlignment(.leading)
+                    .padding(.bottom, 20)
             }
 
-            if let rating = self.mediaListItem.voteAverage {
+            if let rating = self.mediaItem.voteAverage {
                 StarsView(rating: CGFloat(rating/2), maxRating: 5)
                     .frame(width: 100, alignment: .center)
             }
         }
     }
 
-    //    @MainActor
-    //    func getMediaDetailView(mediaType: Watchable.Type) -> some View {
-    //        VStack {
-    //            if (mediaType as? Movie.Type) != nil {
-    //                MovieDetailView(
-    //                    presenter: MovieDetailDefaultPresenter(
-    //                        interactor: DefaultMovieDetailInteractor(),
-    //                        id: self.mediaListItem.id
-    //                    )
-    //                )
-    //            } else {
-    //                TVSerieDetailView(
-    //                    presenter: TVSerieDetailDefaultPresenter(
-    //                        interactor: DefaultTVSerieDetailInteractor(),
-    //                        id: self.mediaListItem.id
-    //                    )
-    //                )
-    //            }
-    //        }
-    //    }
-
-    var body: some View {
-        NavigationLink(
-            destination:
-                Text("Detail") // TODO: - just do it
-            //                getMediaDetailView(mediaType: type(of: mediaListItem))
-        ) {
-            HStack(alignment: .center, spacing: 20) {
-                MoviePosterView(item: mediaListItem)
-                    .frame(maxWidth: 150)
-                movieInfoStackView
-                Spacer()
-            }
-            .frame(height: 250)
+    private func getMediaDetailView() -> some View {
+        VStack {
+            Text("Not implemented yet") // TODO: - just do it
         }
     }
 }
