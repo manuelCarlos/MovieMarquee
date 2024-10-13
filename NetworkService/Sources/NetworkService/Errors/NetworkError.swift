153fc5347ec83ee3d9a9fd84a8ff471f3253a7a1
Manuel Lopes <manuelcarloslopes@icloud.com>
2024-10-13 17:40:17 +0200

    Add NetworkErrorTests

--- NetworkService/Sources/NetworkService/Errors/NetworkError.swift
+++ NetworkService/Sources/NetworkService/Errors/NetworkError.swift
@@ -1,18 +1,11 @@
 //
-//  ApiError.swift
+//  NetworkError.swift
 //  NetworkService
 //
 //  Created by Manuel Lopes on 05.10.24.
 //
 
 import Foundation
-
-enum ApiError: Error {
-    case forbidden              // Status code 403
-    case notFound               // Status code 404
-    case conflict               // Status code 409
-    case internalServerError    // Status code 500
-}
 
 enum NetworkError: LocalizedError {
     case invalidServerResponse
---
+++ NetworkService/Tests/NetworkServiceTests/NetworkErrorTests.swift
@@ -0,0 +1,23 @@
+//
+//  NetworkErrorTests.swift
+//  NetworkServiceTests
+//
+//  Created by Manuel Lopes on 13.10.24.
+//
+
+import XCTest
+
+@testable import NetworkService
+
+final class NetworkErrorTests: XCTestCase {
+    
+    func test_invalid_server_response_error_description() {
+        let error: NetworkError = .invalidServerResponse
+        XCTAssertEqual(error.errorDescription, "The server returned an invalid response.")
+    }
+    
+    func test_invalid_url_error_description() {
+        let error: NetworkError = .invalidURL
+        XCTAssertEqual(error.errorDescription, "URL string is malformed.")
+    }
+}
