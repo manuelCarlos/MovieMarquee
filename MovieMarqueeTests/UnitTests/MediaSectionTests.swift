77b9e836ffa79b4cbe79e0ff244d8659e9732a30
Manuel Lopes <manuelcarloslopes@icloud.com>
2024-10-15 10:57:25 +0200

    Add MediaSectionTests

---
+++ MovieMarqueeTests/UnitTests/MediaSectionTests.swift
@@ -0,0 +1,25 @@
+//
+//  MediaSectionTests.swift
+//  MovieMarquee
+//
+//  Created by Manuel Lopes on 15.10.24.
+//
+
+import XCTest
+
+@testable import MovieMarquee
+
+final class MediaSectionTests: XCTestCase {
+
+    func test_media_section_popular_movies_type_is_movie() {
+        let section = MediaSection.popularMovies
+        XCTAssertEqual(section.type, .movie)
+    }
+
+    func test_media_section_case_iterable_contains_popular_movies() {
+        let allCases = MediaSection.allCases
+
+        XCTAssertEqual(allCases.count, 1)
+        XCTAssertTrue(allCases.contains(.popularMovies))
+    }
+}
