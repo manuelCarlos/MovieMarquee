11fc34b1b0284d774942d12590dcf6836dfc91b4
Manuel Lopes <manuelcarloslopes@icloud.com>
2024-10-15 10:54:06 +0200

    Add ImageExtensionTests

--- MovieMarquee/Extensions/Image+Extensions.swift
+++ MovieMarquee/Extensions/Image+Extensions.swift
@@ -5,17 +5,17 @@
 //  Created by Manuel Lopes on 12.10.24.
 //
 
+import Foundation
 import SwiftUI
 
 extension Image {
 
-    init(resource name: String, ofType type: String) {
-        if let path = Bundle.main.path(forResource: name, ofType: type),
+    init(resource name: String, ofType type: String, in bundle: Bundle = .main) {
+        if let path = bundle.path(forResource: name, ofType: type),
            let image = UIImage(contentsOfFile: path) {
             self.init(uiImage: image)
         } else {
             self.init(systemName: "exclamationmark.triangle")
-            assertionFailure("Couldn't find image named \(name).")
         }
     }
 }
---
+++ MovieMarqueeTests/UnitTests/ImageExtensionTests.swift
@@ -0,0 +1,28 @@
+//
+//  ImageExtensionTests.swift
+//  MovieMarquee
+//
+//  Created by Manuel Lopes on 15.10.24.
+//
+
+import XCTest
+import SwiftUI
+
+@testable import MovieMarquee
+
+final class ImageExtensionTests: XCTestCase {
+
+    private lazy var mockBundle = Bundle(for: type(of: self))
+
+    func test_image_initialization_with_existing_image() {
+        let image = Image(resource: "testImage", ofType: "jpg", in: mockBundle)
+
+        XCTAssertNotNil(image, "Image should not be nil for a valid image resource.")
+    }
+
+    func test_image_initialization_with_non_existing_image() {
+        let image = Image(resource: "nonExistentImage", ofType: "png", in: mockBundle)
+
+        XCTAssertNotNil(image)
+    }
+}
