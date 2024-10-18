ede769b2dc3d323cf82c42402b600a201b764069
Manuel Lopes <manuelcarloslopes@icloud.com>
2024-10-18 12:21:39 +0200

    Adapt Movie to conform to Watchable

--- Models/Sources/Models/Movie.swift
+++ Models/Sources/Models/Movie.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public struct Movie: Codable, Identifiable, Hashable, Sendable {
+public struct Movie: Identifiable, Hashable, Watchable {
     public let id: Int
     public let title: String
     public let adult: Bool?
