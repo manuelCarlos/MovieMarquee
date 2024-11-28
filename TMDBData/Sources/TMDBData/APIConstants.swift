67ee1ce090dd21b3d6c6db493f172e9c5a715d7d
Manuel Lopes <manuelcarloslopes@icloud.com>
2024-11-28 18:34:17 +0100

    Add documentation to public interfaces in the TMDBData package

--- TMDBData/Sources/TMDBData/APIConstants.swift
+++ TMDBData/Sources/TMDBData/APIConstants.swift
@@ -7,19 +7,39 @@
 
 import Foundation
 
+/// A type that holds constants related to The Movie Database (TMDb) API configuration.
+/// This enum provides essential URLs, paths, and configuration values for building API requests.
 public enum APIConstants {
+
+    /// The base domain for TMDb API requests.
     public static let baseURL = "api.themoviedb.org"
+
+    /// The base path for TMDb API endpoints.
+    /// Example full URL: `https://api.themoviedb.org/3`
     public static let baseURLPath = "/3"
 
+    /// The domain for TMDb authentication and web interactions.
     public static let authURL = "www.themoviedb.org"
 
+    /// The scheme used for secure requests.
     public static let httpsScheme = "https"
+
+    /// A default scheme for constructing full URLs.
     public static let defaultScheme = "https://"
 
+    /// The base domain for TMDb image resources.
     public static let baseImgUrl = "image.tmdb.org"
+
+    /// The path for TMDb image resources with a width of 500 pixels.
+    /// Usage: Appended to `baseImgUrl` for constructing image URLs.
     public static let baseImgUrlPath = "/t/p/w500"
 
+    /// The API key used for authenticating TMDb API requests.
+    ///
+    /// This key is loaded from an external source via the `loadAPIKey()` function.
     public static let apiKey = loadAPIKey()
+
+    /// The default language setting for TMDb API requests.
     public static let language = "en-US"
 
     static func loadAPIKey(from bundle: Bundle = .module, jsonFileName: String = "apiKey") -> String? {
