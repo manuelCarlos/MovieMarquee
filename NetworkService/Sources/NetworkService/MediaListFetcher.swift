e7bca65524aa9459afa830aaf65adb917c466c02
Manuel Lopes <manuelcarloslopes@icloud.com>
2024-12-07 20:51:19 +0100

    Document MediaListFetcher protocol

--- NetworkService/Sources/NetworkService/MediaListFetcher.swift
+++ NetworkService/Sources/NetworkService/MediaListFetcher.swift
@@ -1,5 +1,5 @@
 //
-//  MediaListFetchStrategy.swift
+//  MediaListFetcher.swift
 //  NetworkService
 //
 //  Created by Manuel Lopes on 09.10.24.
@@ -7,6 +7,17 @@
 
 import Foundation
 
+/// A protocol for creating `RequestComponents` to fetch paginated media lists.
+///
+/// Conformers of `MediaListFetcher` are responsible for providing the necessary
+/// request details for fetching media data, such as movies, TV shows, or other content.
 public protocol MediaListFetcher {
+
+    /// Generates the `RequestComponents` needed to fetch a specific page of media data.
+    ///
+    /// - Parameter page: The page number to fetch. Typically corresponds to a paginated
+    ///   API's `page` parameter.
+    /// - Returns: A `RequestComponents` instance containing the details of the network request
+    ///   for the specified page. ```
     func fetchRequestComponents(page: Int) -> RequestComponents
 }
