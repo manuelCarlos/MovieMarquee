ee74f15d623c258957737c43bddec77390662c8f
Manuel Lopes <manuelcarloslopes@icloud.com>
2024-12-09 09:12:35 +0100

    Add Spacings enum type to Lego to provide default spacing constants commonly used in UI layout.

---
+++ Lego/Sources/Lego/Spacings.swift
@@ -0,0 +1,42 @@
+//
+//  Spacings.swift
+//  Lego
+//
+//  Created by Manuel Lopes on 09.12.24.
+//
+
+import CoreFoundation
+
+/// Provides default spacing constants commonly used in UI layout.
+public enum Spacings {
+
+    /// Spacing 0.
+    public static let zero: CGFloat = 0
+
+    /// Spacing 2.
+    public static let space2: CGFloat = 2
+
+    /// Spacing 4.
+    public static let space4: CGFloat = 4
+
+    /// Spacing 6.
+    public static let space6: CGFloat = 6
+
+    /// Spacing 8.
+    public static let space8: CGFloat = 8
+
+    /// Spacing 16.
+    public static let space16: CGFloat = 16
+
+    /// Spacing 24.
+    public static let space24: CGFloat = 24
+
+    /// Spacing 28.
+    public static let space28: CGFloat = 28
+
+    /// Spacing 40.
+    public static let space40: CGFloat = 40
+
+    /// Spacing 44.
+    public static let space44: CGFloat = 44
+}
